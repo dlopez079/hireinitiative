3f7aec994a00b4a76033424aa049fef20c36737a
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-26 08:33:18 -0400

    Revised the entire website layout.  Added the pictures and the login-register bar.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,18 +9,22 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Krona One" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" type="text/css">
 
         <!-- Boostrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
         <!-- Inline Styles for Hero, body, HTML, and Headers.  No need to touch.  Boostrap is being used for the rest.-->
         <style>
             body, html {
                 height: 100%;
-                max-width: 100vw;
+                width: 100%;
                 margin: 0px;
                 background-color: lightgrey;
+                font-family: "Raleway", Arial, sans-serif;
             }
+
+            .w3-row img {margin-bottom: -8px}
 
             /* The hero image */
             .hero-image {
@@ -30,8 +34,7 @@
                 /* Set a specific height */
                 height: 100%;
 
-                /* Set a specific width */
-                width: 100vw;
+
 
                 /* Position and center the image to scale nicely on all screens */
                 background-position: center;
@@ -52,11 +55,10 @@
                 color: white;
                 border: 3px solid #f1f1f1;
                 padding: 20px;
-            }
+            }  
+        
+            .row {
 
-            }
-            h1, h2 {
-                text-align: center;
             }
         </style>
 
@@ -76,9 +78,11 @@
     </head>
     <body>
        
-        <!-- Create a placeholder so you can put an image along with a title for the webpage -->
-        <div class="hero-image">
+        <!-- !PAGE CONTENT! -->
+        <div class="w3-content" style="max-width:1500px">
 
+        <!-- Header -->
+        <header class="w3-container w3-xlarge w3-padding-24">
             <!-- Login Register Div -->
             <div id="loginRegisterBar">
                 @if (Route::has('login'))
@@ -95,139 +99,51 @@
                     </div>
                 @endif
             </div>
+        </header>
 
-            <div class="hero-text">
-                <h1>9/11 Memorial Tribute</h1>
-                <h2>H.I.R.E Initiative</h2>
-                <h2>Honesty Integrity Respect and Equality </h2>
+        <!-- Photo Grid -->
+        <div class="w3-row">
+            <div class="w3-half">
+            <img src="/storage/img/Memorial.jpg" style="width:100%">
+            <img src="/storage/img/Memorial001.jpg" style="width:100%">
+            <img src="/storage/img/Memorial002.jpg" style="width:100%">
+            </div>
+
+            <div class="w3-half">
+            <img src="/storage/img/Memorial003.jpg" style="width:100%">
+            <img src="/storage/img/Memorial004.jpg" style="width:100%">
+            <img src="/storage/img/Memorial005.jpg" style="width:100%">
             </div>
         </div>
-        <!-- End of the hero -->
-
-        <!-- Container  -->
-        <div class="container-fluid w-100 p-0 m-0">
-
-            <!-- This is the row that contains the fox 4 footage and Mark's quote -->
-            <div class="row w-100 p-0 m-0">
-
-                <!-- Column with Fox News video -->
-                <div class="col">
-                    <iframe scrolling="no" frameborder="0" allowfullscreen webkitallowfullscreen mozallowfullscreen allow="autoplay; fullscreen" src="https://w3.cdn.anvato.net/player/prod/v3/anvload.html?key=eyJtIjoiRVBGT1giLCJ2IjoiODUwMDIwIiwiYW52YWNrIjoiN3h5S09PSzdYcGNPYkxadTVTN0VhOEJURDdHcFp5dmUiLCJzaGFyZUxpbmsiOiJodHRwczovL3d3dy5mb3g1bnkuY29tL25ld3MvYW1lcmljYS1yZW1lbWJlcnMtOS0xMS12aXJ1cy1hbHRlcnMtZmFtaWxpYXItdHJpYnV0ZXMiLCJwbHVnaW5zIjp7ImN1c3RvbUNvbXNjb3JlUGx1Z2luIjp7ImMzIjoiRk9YIDUgTlkiLCJjNiI6ImZ0cyIsInNjcmlwdCI6Imh0dHBzOi8vc3RhdGljLmZveHR2LmNvbS9zdGF0aWMvb3Jpb24vc2NyaXB0cy9jb3JlL3V0aWxzL2NvbXNjb3JlL0N1c3RvbUNvbXNjb3JlUGx1Z2luLmpzIiwic2RrIjoiaHR0cHM6Ly9zdGF0aWMuZm94dHYuY29tL3N0YXRpYy9vcmlvbi9zY3JpcHRzL2NvcmUvdXRpbHMvY29tc2NvcmUvY29tc2NvcmUtNy4wLjAuMTkxMDE0LmpzIiwiY2xpZW50SWQiOiI2MDQyOTAxIiwibnNfc3Rfc3QiOiJXTllXIiwidGl0bGUiOiI5LzExIFRyaWJ1dGUgaW4gUXVlZW5zIiwibnNfc3RfY2kiOiI4NTAwMjAifSwiY3VzdG9tU2VnbWVudFBsdWdpbiI6eyJzY3JpcHQiOiJodHRwczovL3N0YXRpYy5mb3h0di5jb20vc3RhdGljL29yaW9uL3NjcmlwdHMvY29yZS91dGlscy9DdXN0b21TZWdtZW50UGx1Z2luLmpzIiwicHJpbWFyeV9idXNpbmVzc191bml0IjoiZnRzIiwic2Vjb25kYXJ5X2J1c2luZXNzX3VuaXQiOiJ3bnl3IiwiYXBwX25hbWUiOiJmb3g1bnkuY29tIiwiYXBwX3BsYXRmb3JtIjoid2ViIiwiYXBwX3ZlcnNpb24iOiIxLjAuMCIsInNlZ21lbnRJZCI6InpvSTQ4TkZwSklvT3VaNENxU3g4WlQ2VGRIUW96dFc4In0sImRmcCI6eyJjbGllbnRTaWRlIjp7ImFkVGFnVXJsIjoiaHR0cHM6Ly9wdWJhZHMuZy5kb3VibGVjbGljay5uZXQvZ2FtcGFkL2Fkcz9pdT0vNjM3OTA1NjQvd255d19mb3g1JmRlc2NyaXB0aW9uX3VybD1bcGxhY2Vob2xkZXJdJmVudj12cCZpbXBsPXMmY29ycmVsYXRvcj0mdGZjZD0wJm5wYT0wJmdkZnBfcmVxPTEmb3V0cHV0PXZhc3Qmc3o9MTAwMXgxMDAxJnVudmlld2VkX3Bvc2l0aW9uX3N0YXJ0PTEmY21zaWQ9dW5kZWZpbmVkJnZpZD1BTlZfRVBGT1hfODUwMDIwIiwia2V5VmFsdWVzIjp7InN0eXBlIjpbIm5ld3MiXSwicHR5cGUiOiJ2aWRlby1jbGlwIiwiYyI6WyJuZXdzIiwibWlkZGxlLXZpbGxhZ2UiLCJxdWVlbnMiLCJueWMiLCJzZXB0ZW1iZXItMTEiXSwiZCI6IndlYiJ9fX0sImhlYWx0aEFuYWx5dGljcyI6eyJhY2Nlc3NLZXkiOiI3eHlLT09LN1hwY09iTFp1NVM3RWE4QlREN0dwWnl2ZSIsInBsYXllclZlcnNpb24iOiIzLjUuMiIsInBsYXllckJ1aWx0IjoiMSIsInBsYXllclJldmlzaW9uIjoiM2FiMDVmMyJ9fSwiaHRtbDUiOnRydWUsImZvcm1hdCI6Im0zdTgiLCJ0b2tlbiI6ImV5SmhiR2NpT2lKSVV6STFOaUlzSW5SNWNDSTZJa3BYVkNKOS5leUoyYVdRaU9pSTROVEF3TWpBaUxDSnBjM01pT2lJM2VIbExUMDlMTjFod1kwOWlURnAxTlZNM1JXRTRRbFJFTjBkd1dubDJaU0lzSW1WNGNDSTZNVFl3TVRBME9UTTJPU3dpYVdGMElqb3hOakF4TURRMU56WTVmUS5ab2ZKZDlVY0JqTnpxUE13eFJPSWkybmhJVFlXTFV6WUhra1BaQmE3MjdVIn0%3D"  width ="640" height="360"></iframe>
-                </div>
-
-                <!-- Column with Mark's quote which is vertically and horizontally centered. -->
-                <div class="col m-auto text-center">
-                    <h2>"Honestly, when they started talking about not doing the lights, I walked into my office and told my team we have a job to do."</h2>
-                    <p>- Mark Papadimitriou </p
-                </div>
-            </div>
-
-            <!-- Tribute Speech/Slideshow -->
-            <div class="row w-100 p-0 m-0">
-
-                <!-- This is the column with the 9/11 Speech -->
-                <div class="col m-auto text-center">
-                    <div id="speech" class="textBox">
-                        <h1>9/11 Tribute Speech</h1>
-                        
-                        <p>"Now some of you may know me as outgoing smart funny I could go on but I won’t.</p> 
-                        
-                        <p>Honestly when it comes to speaking from the heart I get nervous.  As I guess many of you would too. You see when you speak from the heart you open yourself up , make yourself vulnerable.  How ever people I am nervous,  I am nervous for all of us.  We see our society changing on a daily basis.  We see nation for our children that we don’t want them to grow up in.  The only way to fix it, is to be the change you are looking for.  I believe Elon musk said it best,” I can either sit back and watch it happen , or I can be a part of it”.  People, be a part of the change you are looking for.  The tribute you see here is provided by an organization called the H.I.R.E initiative. H.I.R.E stands for humanity ?? - Honesty, Integrity, Respect, and Equality. Virtues that if we all adhere to, Can change the world.</p>
-
-                        <p>And so now if you would allow me just a couple more minutes of your time.</p>
-
-                    </div>
-                </div>
-
-                <!-- This is the column with the Slideshow here -->
-                <div class="col">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                        </ol>
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                            <img src="/storage/img/Memorial001-480x360.jpg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                            <img src="/storage/img/Memorial002-480x360.jpg" class="d-block w-100" alt="...">
-                            </div>
-                            <div class="carousel-item">
-                            <img src="/storage/img/Memorial005-480x360.jpg" class="d-block w-100" alt="...">
-                            </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            
-            <!-- Slideshow/Testimonial -->
-            <div class="row w-100 p-0 m-0">
-
-                <!-- Slideshow 2 -->
-                <div class="col">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                <img src="/storage/img/Memorial003-480x640.jpg" class="d-block w-100" alt="...">
-                                </div>
-                                <div class="carousel-item">
-                                <img src="/storage/img/Memorial004-480x640.jpg" class="d-block w-100" alt="...">
-                                </div>
-                                <div class="carousel-item">
-                                <img src="/storage/img/Memorial006-480x640.jpg" class="d-block w-100" alt="...">
-                                </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        </div>
-                    </div>
-                <!-- Tribute to Our city  -->
-                <div class="col m-auto text-center">
-                    <h1>A Tribute to Our City</h1>
-
-                    <p>There are days in our lives that we may forget, and other days which can never and should never be forgotten. September 11, 2001 is one of these days.  America was attacked at its very core by entities which seek to do use harm. They seek to do us harm because America provides a freedom they are not willing to.</p>
-
-                    <p>On that day We as Americans witnessed the worst terror attack in modern history.  We watched as friends, family, loved ones died for no other reason than showing up to work.  So many share stories of loved ones who perished, and others share stories of how they or their loved ones were able, one way or another, to make it home and are grateful for the time they have.</p>
-
-                    <p>We watched as the twin towers crumbled to the ground and our hearts ached that day.  What I saw and watched, following. The days and weeks after 9-11 I saw humans come together side by side giving their time, money, blood, sweat, tears anything to help to overcome what happened. I saw people hugging strangers in the streets. The city of  New York and The nation was transformed into a different society. The kind of society we could only hope for, in these current times.</p>
-
-                    <p>What you see here before you is a reminder to never forget. Never forget where you were when you heard the news , never forget the first phone call you made to a loved one. never forget the joy you felt when  your loved one called you back. Never forget where you sat and cried when they didn’t.  I remember all of these as if it was yesterday and I, we ( all of you out there right now) will never forget. </p>
-
-                    <p>As you stand here with me I ask for a moment of silence, for all those who perished that day and all of the people who have passed since ,due to illness related to 9-11 
-                    So please a moment of silence</p>
-
-                    <p>Thank you to all of you who have come tonight.  I invite you to come up and view this tribute the memory of 9-11.</p>
-                </div>
-                
-            </div>
-
-        </div>
-        <!-- End of Container -->
         
-            
+        <!-- End Page Content -->
+        </div>
+
+        <!-- Footer / About Section -->
+        <footer class="w3-light-grey w3-padding-64 w3-center" id="about">
+        <h2>About</h2>
+        <img src="https://via.placeholder.com/150" class="w3-image w3-padding-32" width="300" height="300">
+        <form style="margin:auto;width:60%" action="/action_page.php" target="_blank">
+            <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
+            <p class="w3-large w3-text-pink">Do not hesitate to contact me!</p>
+            <div class="w3-section">
+            <label><b>Name</b></label>
+            <input class="w3-input w3-border" type="text" required name="Name">
+            </div>
+            <div class="w3-section">
+            <label><b>Email</b></label>
+            <input class="w3-input w3-border" type="text" required name="Email">
+            </div>
+            <div class="w3-section">
+            <label><b>Message</b></label>
+            <input class="w3-input w3-border" required name="Message">
+            </div>
+            <button type="submit" class="w3-button w3-block w3-dark-grey">Send</button>
+        </form>
+        <br>
+        <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+        </footer>
 
 
-    
     </body>
 </html>
