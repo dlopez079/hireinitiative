87947c346da7de9da18b449ab5bb2676c8d1b7a7
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-28 14:00:58 -0400

    Revised template being used for project.  I added the main image and resized.  I changed the color of the navbar.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
         <!-- Inline Styles for Hero, body, HTML, and Headers.  No need to touch.  Boostrap is being used for the rest.-->
-        <style>
+        <style> 
             body, html {
                 height: 100%;
                 width: 100%;
@@ -33,7 +33,7 @@
 
                 /* Set a specific height */
                 height: 100%;
-
+                width: 100%;
 
 
                 /* Position and center the image to scale nicely on all screens */
@@ -77,73 +77,159 @@
 
     </head>
     <body>
-       
-        <!-- !PAGE CONTENT! -->
-        <div class="w3-content" style="max-width:1500px">
-
-        <!-- Header -->
-        <header class="w3-container w3-xlarge w3-padding-24">
-            <!-- Login Register Div -->
-            <div id="loginRegisterBar">
-                @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                        @auth
-                            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                            @endif
-                        @endif
-                    </div>
-                @endif
-            </div>
-        </header>
-
-        <!-- Photo Grid -->
-        <div class="w3-row">
-            <div class="w3-half">
-            <img src="/storage/img/Memorial.jpg" style="width:100%">
-            <img src="/storage/img/Memorial001.jpg" style="width:100%">
-            <img src="/storage/img/Memorial002.jpg" style="width:100%">
-            </div>
-
-            <div class="w3-half">
-            <img src="/storage/img/Memorial003.jpg" style="width:100%">
-            <img src="/storage/img/Memorial004.jpg" style="width:100%">
-            <img src="/storage/img/Memorial005.jpg" style="width:100%">
-            </div>
+            <!-- N5555555555555555555555555avbar (sit on top) -->
+    <div class="w3-top">
+    <div class="w3-bar w3-teal w3-wide w3-padding w3-card">
+        <a href="#home" class="w3-bar-item w3-button"><b>H.I.R.E</b> Initiative </a>
+        <!-- Float links to the right. Hide them on small screens -->
+        <div class="w3-right w3-hide-small">
+        <a href="#projects" class="w3-bar-item w3-button">Projects</a>
+        <a href="#about" class="w3-bar-item w3-button">About</a>
+        <a href="#contact" class="w3-bar-item w3-button">Contact</a>
         </div>
-        
-        <!-- End Page Content -->
-        </div>
+    </div>
+    </div>
 
-        <!-- Footer / About Section -->
-        <footer class="w3-light-grey w3-padding-64 w3-center" id="about">
-        <h2>About</h2>
-        <img src="https://via.placeholder.com/150" class="w3-image w3-padding-32" width="300" height="300">
-        <form style="margin:auto;width:60%" action="/action_page.php" target="_blank">
-            <p>Just me, myself and I, exploring the universe of unknownment. I have a heart of love and an interest of lorem ipsum and mauris neque quam blog. I want to share my world with you. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla. Praesent tincidunt sed tellus ut rutrum. Sed vitae justo condimentum, porta lectus vitae, ultricies congue gravida diam non fringilla.</p>
-            <p class="w3-large w3-text-pink">Do not hesitate to contact me!</p>
-            <div class="w3-section">
-            <label><b>Name</b></label>
-            <input class="w3-input w3-border" type="text" required name="Name">
-            </div>
-            <div class="w3-section">
-            <label><b>Email</b></label>
-            <input class="w3-input w3-border" type="text" required name="Email">
-            </div>
-            <div class="w3-section">
-            <label><b>Message</b></label>
-            <input class="w3-input w3-border" required name="Message">
-            </div>
-            <button type="submit" class="w3-button w3-block w3-dark-grey">Send</button>
+    <!-- Header -->
+    <header class="w3-display-container w3-content w3-wide" style="max-width:100%;" id="home">
+    <img class="w3-image" src="/storage/img/Memorial.jpg" alt="Architecture" width="100%" height="100%">
+    <div class="w3-display-middle w3-margin-top w3-center">
+        <h1 class="w3-xxxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min"><b>H.I.R.E</b></span> <span class="w3-hide-small w3-text-yellow">Initiative</5pan></h1>
+    </div>
+    </header>
+
+    <!-- Page content -->
+    <div class="w3-content w3-padding" style="max-width:1564px">
+
+    <!-- Project Section -->
+    <div class="w3-container w3-padding-32" id="projects">
+        <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h3>
+    </div>
+
+    <div class="w3-row-padding">
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Summer House</div>
+            <img src="/w3images/house5.jpg" alt="House" style="width:100%">
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Brick House</div>
+            <img src="/w3images/house2.jpg" alt="House" style="width:100%">
+        </div>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
+            <img src="/w3images/house3.jpg" alt="House" style="width:100%">
+        </div>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
+            <img src="/w3images/house4.jpg" alt="House" style="width:100%">
+        </div>
+        </div>
+    </div>
+
+    <div class="w3-row-padding">
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Summer House</div>
+            <img src="/w3images/house2.jpg" alt="House" style="width:99%">
+        </div>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Brick House</div>
+            <img src="/w3images/house5.jpg" alt="House" style="width:99%">
+        </div>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Renovated</div>
+            <img src="/w3images/house4.jpg" alt="House" style="width:99%">
+        </div>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <div class="w3-display-container">
+            <div class="w3-display-topleft w3-black w3-padding">Barn House</div>
+            <img src="/w3images/house3.jpg" alt="House" style="width:99%">
+        </div>
+        </div>
+    </div>
+
+    <!-- About Section -->
+    <div class="w3-container w3-padding-32" id="about">
+        <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">About</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+    </div>
+
+    <div class="w3-row-padding w3-grayscale">
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="/w3images/team2.jpg" alt="John" style="width:100%">
+        <h3>Mark Papadimitriou</h3>
+        <p class="w3-opacity">CEO & Founder</p>
+        <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+        <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="/w3images/team1.jpg" alt="Jane" style="width:100%">
+        <h3>Jane Doe</h3>
+        <p class="w3-opacity">Architect</p>
+        <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+        <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="/w3images/team3.jpg" alt="Mike" style="width:100%">
+        <h3>Mike Ross</h3>
+        <p class="w3-opacity">Architect</p>
+        <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+        <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+        </div>
+        <div class="w3-col l3 m6 w3-margin-bottom">
+        <img src="/w3images/team4.jpg" alt="Dan" style="width:100%">
+        <h3>Dan Star</h3>
+        <p class="w3-opacity">Architect</p>
+        <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+        <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+        </div>
+    </div>
+
+    <!-- Contact Section -->
+    <div class="w3-container w3-padding-32" id="contact">
+        <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Contact</h3>
+        <p>Lets get in touch and talk about your next project.</p>
+        <form action="/action_page.php" target="_blank">
+        <input class="w3-input w3-border" type="text" placeholder="Name" required name="Name">
+        <input class="w3-input w3-section w3-border" type="text" placeholder="Email" required name="Email">
+        <input class="w3-input w3-section w3-border" type="text" placeholder="Subject" required name="Subject">
+        <input class="w3-input w3-section w3-border" type="text" placeholder="Comment" required name="Comment">
+        <button class="w3-button w3-black w3-section" type="submit">
+            <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
         </form>
-        <br>
-        <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank" class="w3-hover-text-green">w3.css</a></p>
-        </footer>
+    </div>
+    
+    <!-- Image of location/map -->
+    <div class="w3-container">
+    <img src="/w3images/map.jpg" class="w3-image" style="width:100%">
+    </div>
+
+    <!-- End page content -->
+    </div>
 
 
+    <!-- Footer -->
+    <footer class="w3-center w3-black w3-padding-16">
+    <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+    </footer>
+
+</body>
+</html>
+    
     </body>
 </html>
